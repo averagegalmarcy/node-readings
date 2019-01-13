edf594299cf4cf9dd449fa4354058e24c854586c
Marcy Silverman <marcysilve225@gmail.com>
2019-01-13 10:31:50 -0800

    started on event handlers

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require("http");
+var url = require("url");
 
 // http.createServer(function(request, response) {
 //   response.writeHead(200, { "Content-Type": "text/plain" });
@@ -15,18 +16,31 @@ var http = require("http");
 // http.createServer(onRequest).listen(8888);
 // console.log('server has started');
 
-function start() {
-  function onRequest(request, response) {
-    console.log("requested received");
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Hello World 2");
-    response.end();
-  }
-  http.createServer(onRequest).listen(8888);
-  console.log("server has started");
-}
-exports.start = start;
+// function start(route) {
+//   function onRequest(request, response) {
+//     var pathname = url.parse(request.url).pathname;
+//     console.log("requested for" + pathname  + "received");
+//     route(pathname);
+//     response.writeHead(200, { "Content-Type": "text/plain" });
+//     response.write("Hello World 2");
+//     response.end();
+//   }
+//   http.createServer(onRequest).listen(8888);
+//   console.log("server has started");
+// }
+// exports.start = start;
 // database.query("SELECT * FROM hugetable", rows => {
 //    var result = rows;
 // });
 // console.log('Hello World');
+
+function start() {
+  console.log("request handler 'start' was called");
+}
+
+function upload() {
+  console.log("requested handler upload was called");
+}
+
+exports.start = start;
+exports.upload = upload;
